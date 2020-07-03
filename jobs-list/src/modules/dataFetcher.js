05b020f40e62feb2c6d88a3193fa2cca935347f3
anton <anton@blackhorseteam.com>
2020-07-03 18:08:40 +0300

    Добавил метод data Fetcher

---
+++ jobs-list/src/modules/dataFetcher.js
@@ -0,0 +1,10 @@
+export function fetchVacancies() {
+    // TODO: вернуть после того как заапрувят ключи в hh.ru
+    /*
+    return fetch('MEGA_HH_URL').then(...)
+    */
+    return Promise.resolve([
+   // {...}, // тут объекты будут с такой же схемой как hh отдает.
+   // {...}
+    ]);
+   }
